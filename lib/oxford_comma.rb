b7889590d1fa352d87e18f7b175185883eee26eb
sbalayan1 <balayans2014@yahoo.com>
2021-06-21 14:45:30 -0700

    commit tree 8dc9a9c29f25eae72a5fe0f4d848e4dd0a6a930c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+test_array = ['sean', 'gimelle', 'arthur']
+
 def oxford_comma(array)
 
+    if array.size == 1
+        array.join
+    elsif array.size == 2 
+        array.join(" and ")
+    else array.size > 2 
+        lastItem = array[-1]
+        array.pop
+        array << ("and #{lastItem}")
+        array.join(", ")
+    end
 end
